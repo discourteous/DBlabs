ed6b43efdaadba25f5a4fea9632c4a2a6d95e439
Vladimir Dotsenko <114189378+discourteous@users.noreply.github.com>
2023-05-26 01:02:01 +0300

    Add files via upload

--- lab5.sql
+++ lab5.sql
@@ -1,49 +1,34 @@
-SELECT cities.name,  regions.name
-FROM cities, regions
-WHERE cities.region=regions.uuid AND population >350000
+SELECT region, SUM(population) AS region_population
+FROM cities
+GROUP BY region;
 
-SELECT cities.name,  regions.name
-FROM cities, regions
-WHERE cities.region=regions.uuid AND regions.name = 'Nord' 
+SELECT region, SUM(population) AS region_population
+FROM cities
+GROUP BY region
+HAVING COUNT(name) >= 10;
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
+SELECT name, population
+FROM cities
+WHERE region IN (SELECT uuid
+                  FROM regions
+                  WHERE area_quantity >= 5) 
+ORDER BY population DESC
+LIMIT 5 OFFSET 10;
 
-DROP TABLE IF EXISTS `Line`;
-CREATE TABLE `Line` (
-  `id` int(20) NOT NULL,
-  `name` char(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SELECT region, SUM(population) as total_population
+FROM cities
+WHERE population > 300000
+GROUP BY region;
 
-INSERT INTO `line` (`id`, `name`) VALUES
-(1,	'green'),
-(2,	'red'),
-(3,	'blue');
+SELECT name, 
+       (SELECT SUM(population) 
+        FROM cities 
+        WHERE region = regions.uuid AND population > 300000) as total_population
+FROM regions;
 
-DROP TABLE IF EXISTS `station`;
-CREATE TABLE `station` (
-  `id` int(10) NOT NULL,
-  `name_of_station` char(20) NOT NULL,
-  `number_of_line` int(20) NOT NULL,
-  `number_of_station_in_line` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `station_of_metro` (`id_of_station`, `name_of_station`, `number_of_line`, `number_of_station_in_line`) VALUES
-(1,	'HolodnaGora',	2,	1),
-(2,	'GeroivPraci',  3,	1),
-(3,	'Peremoga',	1,	1),
-(4,     'Zavod im Malishev',	2, 	7),
-(5,     'Akademika Barabashova',	3,	4),
-(6,     'Naukova',	1,	4),
-(7,	'Indystialna',	2,	13),
-(8,	'IstorishniiMysei',	3,	8),
-(9,	'MetroBudivnikiv',	1,	9);
-
-DROP TABLE IF EXISTS `transfer_on_line_to_line`;
-CREATE TABLE `transfer_on_line_to_line` (
-  `from_station` int(20) NOT NULL,
-  `to_station` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `transfer_on_line_to_line` (`from_line`, `to_line`, `from_station`, `to_station`) VALUES
-(4,	1);
+SELECT name, population
+FROM cities
+WHERE region IN (SELECT uuid 
+                  FROM regions
+                  WHERE area_quantity <= 5)
+AND population > 150000 AND population < 500000
